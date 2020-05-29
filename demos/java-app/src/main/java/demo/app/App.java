2f7f61944bd11e93865bfdc0ccc8c9150f8a0e39
Marek Dudek <marek.dudek@gmail.com>
2020-05-29 21:11:52 +0200

    cosmetic changes

--- demos/java-app/src/main/java/demo/app/App.java
+++ demos/java-app/src/main/java/demo/app/App.java
@@ -2,11 +2,11 @@ package demo.app;
 
 public class App {
 
-    public String getGreeting() {
-        return "Hello world.";
-    }
-
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+    }
+
+    public String getGreeting() {
+        return "Hello world.";
     }
 }
