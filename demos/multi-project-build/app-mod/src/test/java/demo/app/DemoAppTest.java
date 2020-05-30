386022a8ff6e5e9e9fdbf9fd14ba2d87fec97c56
Marek Dudek <marek.dudek@gmail.com>
2020-05-30 23:11:18 +0200

    some common things

---
+++ demos/multi-project-build/app-mod/src/test/java/demo/app/DemoAppTest.java
@@ -0,0 +1,11 @@
+package demo.app;
+
+import org.junit.Test;
+
+public class DemoAppTest {
+
+    @Test
+    public void test() {
+        new DemoApp().main(null);
+    }
+}
---
+++ demos/multi-project-build/library-mod/src/test/java/demo/lib/SomeUtilTest.java
@@ -0,0 +1,11 @@
+package demo.lib;
+
+import org.junit.Test;
+
+public class SomeUtilTest {
+
+    @Test
+    public void test() {
+        new SomeUtil().doSomething();
+    }
+}
