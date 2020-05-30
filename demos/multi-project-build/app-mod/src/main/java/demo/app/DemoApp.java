939db27a2c68852eebf19c91f58b034143c2886d
Marek Dudek <marek.dudek@gmail.com>
2020-05-30 23:01:27 +0200

    multip-project build works

---
+++ demos/multi-project-build/app-mod/src/main/java/demo/app/DemoApp.java
@@ -0,0 +1,11 @@
+package demo.app;
+
+import demo.lib.SomeUtil;
+
+public class DemoApp {
+
+    public static void main(String[] args) {
+        System.out.println("This is Demo App");
+        new SomeUtil().doSomething();
+    }
+}
---
+++ demos/multi-project-build/library-mod/src/main/java/demo/lib/SomeUtil.java
@@ -0,0 +1,8 @@
+package demo.lib;
+
+public class SomeUtil {
+
+    public void doSomething() {
+        System.out.println("Util is doing something");
+    }
+}
